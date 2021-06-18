d779afbe746ad1e1ee2518a7ceaa28e731f470f6
mxr <eetu.kivelae@gmail.com>
2021-06-18 16:26:33 +0300

    Feature: Added niilo command functionality Added basic functionality to niilo command to make it load a quote from a webserver.

--- src/commands/niilo.go
+++ src/commands/niilo.go
@@ -1,15 +1,39 @@
 package commands
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
+
+type niiloRes struct {
+	Id     string `json:"_id"`
+	Text   string
+	Number uint64
+}
 
 var niiloInfo = discordgo.ApplicationCommand{
 	Name:        "niilo",
 	Description: "Post a niilo quote",
 }
 
+var client = &http.Client{Timeout: 10 * time.Second}
+
 func niiloCmd(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	res, err := client.Get("https://misi.mxrr.dev/api/v1/niilo")
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer res.Body.Close()
+
+	quote := new(niiloRes)
+	json.NewDecoder(res.Body).Decode(&quote)
+	log.Print(quote)
+
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionApplicationCommandResponseData{
