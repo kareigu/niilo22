2cb81ef94cbfbefcd37b249a3bbc271375a634f9
mxr <eetu.kivelae@gmail.com>
2021-06-20 15:36:52 +0300

    Feature: Added postData function Is used to post data to a server.

--- src/commands/main.go
+++ src/commands/main.go
@@ -22,13 +22,39 @@ var (
 
 var client = &http.Client{Timeout: 10 * time.Second}
 
+const UserAgent = "Niilo22-Bot/1.0"
+
 func getData(url string, target interface{}) error {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return err
 	}
 
-	req.Header.Set("User-Agent", "Niilo22-Bot/1.0")
+	req.Header.Set("User-Agent", UserAgent)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	return json.NewDecoder(res.Body).Decode(&target)
+}
+
+func postData(url string, target interface{}, body interface{}) error {
+	json_body, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json_body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Content-Type", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
